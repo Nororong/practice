9f375e99b930ec41dbeea662fc7940c595172bb0
wlsdnjs1022 <wlsdnjs1022@naver.com>
2025-12-23 17:15:16 +0900

    1463 해겨ㄹ

--- Algorithm/problem_3986/problem_3986.c
+++ Algorithm/problem_3986/problem_3986.c
@@ -1,40 +1,35 @@
-#define	_CRT_SECURE_NO_WARNINGS
+# define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-
-typedef struct {
-	char name;
-} array;
 
 int main() {
 	int tc;
 	scanf("%d", &tc);
-
-	int count = 0;
-
+	int count=0;
 	while (tc--) {
-		char str[100001];
-		scanf("%s", str);
-
-		int len = (int)strlen(str);
-		
-		array stack[100001];
-		
+		char str[10001];
 		int top = -1;
 
+		scanf("%s", &str);
+
+		char stack[100001];
+
+		int len = (int)strlen(str);
+
 		for (int i = 0; i < len; i++) {
-			char now = str[i];
-				if (top>=0 && (stack[top].name == now)) {
-					top--;
-				}
-				else {
-					stack[++top].name = now;
-				}
+			char word = str[i];
+
+			if (top >= 0 && stack[top] == word) {
+				top--;
 			}
+			else {
+				stack[++top] = word;
+			}
+		}
 		if (top == -1) {
 			count++;
 		}
 	}
-	printf("%d", count);
+	printf("%d\n", count);
 	return 0;
 }
